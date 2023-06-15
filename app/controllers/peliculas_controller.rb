00d794abc83147af046242e5105dade9ff376a6c
Francisca Olivares <francisca.olivares.vecchiola@gmail.com>
2023-06-15 10:55:16 -0400

    Se terminaron las vistas, y se completo la vista de la pelicula 1, quedan pendientes las demas

---
+++ app/controllers/peliculas_controller.rb
@@ -0,0 +1,40 @@
+class PeliculasController < ApplicationController
+    def pelicula1
+        render layout: 'vistaslayout'
+    end
+    def pelicula2
+        render layout: 'vistaslayout'
+    end
+    def pelicula3
+        render layout: 'vistaslayout'
+    end
+    def pelicula4
+        render layout: 'vistaslayout'
+    end
+    def pelicula5
+        render layout: 'vistaslayout'
+    end
+    def pelicula6
+        render layout: 'vistaslayout'
+    end
+    def pelicula7
+        render layout: 'vistaslayout'
+    end
+    def pelicula8
+        render layout: 'vistaslayout'
+    end
+    def pelicula9
+        render layout: 'vistaslayout'
+    end
+    def pelicula10
+        render layout: 'vistaslayout'
+    end
+    def pelicula11
+        render layout: 'vistaslayout'
+    end
+    def pelicula12
+        render layout: 'vistaslayout'
+    end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#index"
+  get '/el_cisne_negro', to: 'peliculas#pelicula1'
+  get '/django_sin_cadenas', to: 'peliculas#pelicula2'
+  get '/matrix_recargado', to: 'peliculas#pelicula3'
+  get '/forest_gump', to: 'peliculas#pelicula4'
+  get '/kill_bill_volumen_1', to: 'peliculas#pelicula5'
+  get '/los_otros', to: 'peliculas#pelicula6'
+  get '/the_matrix', to: 'peliculas#pelicula7'
+  get '/bastardos_sin_loria', to: 'peliculas#pelicula8'
+  get '/perros_de_la_calle', to: 'peliculas#pelicula9'
+  get '/pulp_fiction', to: 'peliculas#pelicula10'
+  get '/el_padrino', to: 'peliculas#pelicula11'
+  get '/el_silencio_de_los_nocentes', to: 'peliculas#pelicula12'
+
 end
